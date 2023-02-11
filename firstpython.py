be5c98299498cfed775ceb364fbdaa29ef777cf0
Dipto Mesbah <81214255+DIptoMesbah@users.noreply.github.com>
2023-02-11 10:32:07 +0600

    Create firstpython.py
    This is a python script created on GitHub. The purpose of this program is to demonstrate how to create a new file in GitHub under a repository.

---
+++ firstpython.py
@@ -0,0 +1,4 @@
+# This code will print a line
+# This code was written by following along a video tutorial for the following course: Tools for Data Science - Week 2 - Github - Getting Started.
+
+print("New Python File")
